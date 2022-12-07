2aaa457e332c575edcf64f0fbc67bb5f9d96b293
ratomsky <max.work.dev@gmail.com>
2022-12-07 21:40:59 +0300

    f

--- src/utils/findOneParams.ts
+++ src/utils/findOneParams.ts
@@ -1,12 +1,5 @@
-import { IsNumber } from 'class-validator';
-import { Transform } from 'class-transformer';
-import { ApiProperty } from '@nestjs/swagger';
-
 export class FindOneParams {
 
-    @ApiProperty()
-    @IsNumber()
-    @Transform(({ value }) => Number(value))
     id: number;
 }
 
